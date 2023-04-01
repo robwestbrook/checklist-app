d4985016a0e8dd60b8433b6a0e6256852665f006
Rob Westbrook <brorobw@gmail.com>
2023-04-01 18:35:52 -0500

    added todo input and todo section

---
+++ src/lib/components/todo/TodoSection.svelte
@@ -0,0 +1,10 @@
+<script>
+  import TodoInput from "./TodoInput.svelte";
+  import { todos } from "../../store/todoStore";
+</script>
+
+<section class="mt-8 space-y-4">
+  {#each $todos as todo (todo.id)}
+    <TodoInput {todo} />
+  {/each}
+</section>
