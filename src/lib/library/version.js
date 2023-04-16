934057d55ca5297b4178683d9e485c3318d171b6
Rob Westbrook <brorobw@gmail.com>
2023-04-16 00:05:29 -0500

    bumped to version 1.0.1

--- src/lib/library/version.js
+++ src/lib/library/version.js
@@ -2,7 +2,7 @@ export const version = {
   dbName: "rememberDB",
   dbBackupName: `rememberDB-backup-${new Date().toLocaleDateString()}.json`,
   copyrightDate: "2023",
-  versionNumber: "1.0",
+  versionNumber: "1.0.1",
   versionDate: "Apr 15, 2023",
   githubUrl: "https://github.com/robwestbrook/checklist-app",
 };
