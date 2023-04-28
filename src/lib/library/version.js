94260649f5bd8db55d851763db5e254c9794cef7
Rob Westbrook <brorobw@gmail.com>
2023-04-28 17:54:51 -0500

    merged routing branch

--- src/lib/library/version.js
+++ src/lib/library/version.js
@@ -2,7 +2,7 @@ export const version = {
   dbName: "rememberDB",
   dbBackupName: `rememberDB-backup-${new Date().toLocaleDateString()}.json`,
   copyrightDate: "2023",
-  versionNumber: "1.1.1",
-  versionDate: "Apr 16, 2023",
+  versionNumber: "1.9.0",
+  versionDate: "Apr 28, 2023",
   githubUrl: "https://github.com/robwestbrook/checklist-app",
 };
